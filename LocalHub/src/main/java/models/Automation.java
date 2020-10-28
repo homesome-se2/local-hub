59e2bb2e6fc3451663ac1f07d1ef7f67a1df2392
jonasjoh97 <45165180+jonasjoh97@users.noreply.github.com>
2020-10-28 15:19:34 +0100

    Added variables and getters and setters to the automations class

--- LocalHub/src/main/java/models/Automation.java
+++ LocalHub/src/main/java/models/Automation.java
@@ -1,5 +1,45 @@
 package models;
 
+
+//class represents a simple automation object of when the masterGadget reaches a
+//certain state, a slaveGadget changes to the set state
 public class Automation {
-    //TODO
+
+    private int masterId;
+    private int slaveId;
+    private float masterState;
+    private float slaveState;
+
+    public int getMasterId() {
+        return masterId;
+    }
+
+    public void setMasterId(int masterId) {
+        this.masterId = masterId;
+    }
+
+    public int getSlaveId() {
+        return slaveId;
+    }
+
+    public void setSlaveId(int slaveId) {
+        this.slaveId = slaveId;
+    }
+
+    public float getMasterState() {
+        return masterState;
+    }
+
+    public void setMasterState(float masterState) {
+        this.masterState = masterState;
+    }
+
+    public float getSlaveState() {
+        return slaveState;
+    }
+
+    public void setSlaveState(float slaveState) {
+        this.slaveState = slaveState;
+    }
+
 }
